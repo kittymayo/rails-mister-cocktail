1548f6203c021d0620cb6f0aa857cd484011fa25
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-02-21 15:14:28 +0000

    methods created for cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,25 +1,41 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
   end
 
   def destroy
+    @cocktail.destroy
   end
 
-  def shhow
+  def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def edit
+    @cocktail = Cocktail.find(params[:id])
   end
 
-  def _form
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :doses)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,45 @@
 class DosesController < ApplicationController
-  def index
-  end
+  # def index
+  #   @doses = Dose.all
+  # end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:ingredient_id])
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.ingredient = Ingredient.find(params[:ingredient_id])
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
   end
 
   def update
+    @dose = Dose.find(params[:id])
+    @dose.update
   end
 
   def destroy
+    @dose.destroy
   end
 
-  def shhow
+  def show
+    @dose = Dose.find(params[:id])
   end
 
   def edit
+    @dose = Dose.find(params[:id])
   end
 
-  def _form
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
