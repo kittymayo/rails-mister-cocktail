b7aa6c1229eaac180a77346671290c259c554821
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-02-22 11:20:16 +0000

    added button to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # root to:  '/cocktails'
   delete '/doses/:id', to: 'doses#destroy', as: :delete_dose
-  resources :cocktails do
-    resources :doses, only: [:new, :create, :destroy]
+  resources :cocktails, except: [:index] do
+    resources :doses, only: [:new, :create]
     resources :ingredients, only: [:new, :create, :destroy]
   end
+  root to: "cocktails#index"
 end
